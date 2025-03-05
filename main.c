05e946aa0a0f4f4edb85aa90d20dc64892679a95
EthanTerrill <ethanrobertterrill@gmail.com>
2025-03-05 08:27:39 -0800

    smaall

--- main.c
+++ main.c
@@ -29,8 +29,43 @@
 
 #include "file_system.h"
 
-#define HTTPS_PORT 8080
+#define HTTPS_PORT 443
 
+
+void print_ssl_error(int error){
+  switch (error) {
+    case SSL_ERROR_NONE:
+      fprintf(stderr, "no error");
+    break;
+  case SSL_ERROR_ZERO_RETURN:
+      fprintf(stderr, "zero return");
+    break; 
+  case SSL_ERROR_WANT_READ:
+      fprintf(stderr, "want read");
+    break; 
+  case SSL_ERROR_WANT_CLIENT_HELLO_CB:
+      fprintf(stderr, "want client hello cb");
+    break; 
+  case SSL_ERROR_WANT_CONNECT:
+      fprintf(stderr, "want connect");
+    break; 
+  case SSL_ERROR_WANT_X509_LOOKUP:
+      fprintf(stderr, "want x509 lookup");
+    break; 
+  case SSL_ERROR_WANT_ASYNC:
+      fprintf(stderr, "want async");
+    break; 
+  case SSL_ERROR_WANT_ASYNC_JOB:
+      fprintf(stderr, "want async job");
+    break;
+  case SSL_ERROR_SYSCALL:
+      fprintf(stderr, "syscall");
+    break;
+  case SSL_ERROR_SSL:
+    fprintf(stderr, "ERROR SSL");
+  }
+  fprintf(stderr, "------\n");
+}
 
 
 int main(int argnum, char** argv) {
@@ -39,11 +74,12 @@ int main(int argnum, char** argv) {
   struct sockaddr_in  my_sockaddr;
   socklen_t           addr_len = sizeof(my_sockaddr);
   int opt = 1;
+  char                *default_opt = "content";
 
 
   web_str client_input;
 
-  if (argnum != 2) {
+  if (argnum != 2 && argnum != 1) {
     fprintf(stderr, "syntax is: main [path]\n");
     exit(EXIT_FAILURE);
   }
@@ -87,44 +123,63 @@ int main(int argnum, char** argv) {
   }
 
   FileSystem* fs;
-  populate_file_system(argv[1], &fs);
+  if(argnum == 2) {
+    populate_file_system(argv[1], &fs);
+  } else if(argnum == 1) {
+    populate_file_system(default_opt, &fs);
+  } else {
+    exit(EXIT_FAILURE); 
+  }
+
+
   print_file_system(fs);
 
   while (1) {
     new_socket = accept(socket_fd, NULL, NULL);
-    fprintf(stdout, "accepted socket\n");
     if (new_socket == -1) {
       fprintf(stderr, "accept socket \n");
       fprintf(stderr, "%s\n", strerror(errno));
       exit(EXIT_FAILURE);
     }
+
+    fprintf(stdout, "accepted socket\n");
     SSL_CTX* context = SSL_CTX_new(TLS_method());
+    if (context == NULL) {
+      fprintf(stderr, "could not create new SSL pointer\n");
+      exit(EXIT_FAILURE); 
+    } 
+
+
     SSL* ssl = SSL_new(context);
+    if (ssl == NULL) {
+      fprintf(stderr, "could not create new SSL pointer %d\n", SSL_get_error(ssl, result));
+      exit(EXIT_FAILURE); 
+    }
+    
     result = SSL_set_fd(ssl, new_socket);
     if (result != 1) {
       fprintf(stderr, "Could not set SSL socket_fd %d\n", SSL_get_error(ssl, result));
       exit(EXIT_FAILURE); 
     }
-    result = SSL_use_certificate_file(ssl, "public.pem", SSL_FILETYPE_PEM);
+    result = SSL_use_certificate_file(ssl, "crypt/myKey.crt", SSL_FILETYPE_PEM);
     if (result != 1) {
-      fprintf(stderr, "Could not use cert chain flle %d\n", result);
+      fprintf(stderr, "Could not use cert chain file %d\n", result);
       exit(EXIT_FAILURE); 
     }
-    result = SSL_use_PrivateKey_file(ssl, "private.pem", SSL_FILETYPE_PEM);
+    result = SSL_use_PrivateKey_file(ssl, "crypt/mykey.key", SSL_FILETYPE_PEM);
     if (result != 1) {
       fprintf(stderr, "Could not use private file %d\n", SSL_get_error(ssl, result));
       exit(EXIT_FAILURE); 
     }
      
 
-    
-
-
     result = SSL_accept(ssl);
     if (result != 1) {
+      print_ssl_error(SSL_get_error(ssl, result));
       fprintf(stderr, "Could not perform SSL handshake %d\n", SSL_get_error(ssl, result));
       exit(EXIT_FAILURE); 
     }
+
     fprintf(stdout, "accepted ssl\n");
     int size = 8000;
     client_input.length = size;
